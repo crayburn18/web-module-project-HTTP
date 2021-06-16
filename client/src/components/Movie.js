4b222e36caf809d359c5202abd14d3bef8ced998
Connor Rayburn <connorrayburn.work@gmail.com>
2021-06-16 14:15:52 -0700

    completed mvp

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -52,7 +52,7 @@ const Movie = (props) => {
                         <section>
                             <span className="m-2 btn btn-dark">Favorite</span>
                             <Link to={`/movies/edit/${movie.id}`} className="m-2 btn btn-success">Edit</Link>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            <span className="delete" onClick={()=>props.deleteMovie(id)}><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
                         </section>
                     </div>
                 </div>
